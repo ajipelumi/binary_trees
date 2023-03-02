96bd09517df3dfa97f175f6a6776137abad9d047
ajipelumi <ajisafeoluwapelumi@gmail.com>
2023-03-02 17:21:47 +0100

    tree leaves

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -18,6 +18,7 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 		return (0);
 	}
 
+	/* left and right point to NULL which indicate a leaf */
 	if (tree->left == NULL && tree->right == NULL)
 	{
 		return (1);
