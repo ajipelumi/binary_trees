8528831618902cb8b39dba65cb66b39b46b27b9c
ajipelumi <ajisafeoluwapelumi@gmail.com>
2023-03-02 17:20:14 +0100

    leaves

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,32 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_leaves - count the leaves of a binary tree
+ *
+ * @tree: a pointer to the root node of the tree to count the leaves
+ *
+ * Return: tree leaves, 0 if tree is NULL
+ */
+
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	size_t leaves = 0;
+
+	/* Base case */
+	if (tree == NULL)
+	{
+		return (0);
+	}
+
+	if (tree->left == NULL && tree->right == NULL)
+	{
+		return (1);
+	}
+
+	/* increment leaves for every recursive call to left subtree */
+	leaves += binary_tree_leaves(tree->left);
+	/* increment leaves for every recursive call to right subtree */
+	leaves += binary_tree_leaves(tree->right);
+
+	return (leaves); /* return leaves of tree */
+}
