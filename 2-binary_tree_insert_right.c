b8fba52cab88bc3b2bdde1f0d254cca083f2bde9
ajipelumi <ajisafeoluwapelumi@gmail.com>
2023-03-02 03:51:43 +0100

    insert left and right

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,15 +1,15 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_left - inserts a node as the left-child of another node
+ * binary_tree_insert_right - inserts a node as the right-child of another node
  *
- * @parent: pointer to the node to insert the left-child in
+ * @parent: pointer to the node to insert the right-child in
  * @value: the value to put in the new node
  *
  * Return: a pointer to the created node, NULL on failure or if parent is NULL
  */
 
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node, *temp;
 
@@ -30,16 +30,16 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	new_node->parent = parent; /* store parent node */
 	new_node->left = new_node->right = NULL;
 
-	/* if left of parent points to a node already */
-	if (parent->left != NULL)
+	/* if right of parent points to a node already */
+	if (parent->right != NULL)
 	{
-		temp = parent->left; /* temp points to node to be replaced */
+		temp = parent->right; /* temp points to node to be replaced */
 		temp->parent = new_node; /* parent of temp points to new node */
-		new_node->left = temp; /* left of new node points to temp */
+		new_node->right = temp; /* right of new node points to temp */
 	}
 
-	/* left of parent now points to new node */
-	parent->left = new_node;
+	/* right of parent now points to new node */
+	parent->right = new_node;
 
 	return (new_node);
 }
