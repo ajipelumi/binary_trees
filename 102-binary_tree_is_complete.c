f7b40189ba33a0ae855dec88d1fcef0ec038c3bd
ajipelumi <ajisafeoluwapelumi@gmail.com>
2023-03-04 03:09:12 +0100

    complete tree

---
+++ 102-binary_tree_is_complete.c
@@ -0,0 +1,53 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_complete - checks if a binary tree is complete
+ *
+ * @tree: a pointer to the root node of the tree to check
+ *
+ * Return: 1 if tree is complete, 0 otherwise
+ */
+
+int binary_tree_is_complete(const binary_tree_t *tree)
+{
+	binary_tree_t **queue = NULL;
+	int head = 0, tail = 0, size = 0, flag = 0, i;
+
+	if (tree == NULL) /* if tree is NULL */
+		return (0);
+
+	queue = malloc(sizeof(*queue) * 1024); /* create queue to store tree items */
+	if (queue == NULL) /* malloc fails */
+		return (0);
+
+	queue[0] = (binary_tree_t *) tree; /* add root node to queue */
+	tail = size = 1; /* tail and size increases by 1 */
+	while (head < size) /* iterate over queue */
+	{
+		for (i = head; i < size; i++)
+		{
+			if (queue[i]->left) /* check if a left child exists */
+			{
+				if (flag == 1) /* implies that the right of the prev node is not full */
+					return (0);
+				queue[tail] = queue[i]->left; /* last item in queue now points to left */
+				tail++; /* tail increases */
+			}
+			else /* no left child */
+				flag = 1;
+			if (queue[i]->right) /* check if a right child exists */
+			{
+				if (flag == 1) /* implies that the left of the prev node is not full */
+					return (0);
+				queue[tail] = queue[i]->right; /* last item in queue now points to right */
+				tail++; /* tail increases */
+			}
+			else /* no right child */
+				flag = 1;
+		}
+		head = size;
+		size = tail;
+	}
+	free(queue); /* free queue */
+	return (flag); /* return flag */
+}
